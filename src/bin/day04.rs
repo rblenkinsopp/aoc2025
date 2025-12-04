9b1ce1cc22194c1c043e1e78726ccee43ef0bf91
Robert Blenkinsopp <robert@blenkinsopp.net>
2025-12-04 09:18:20 +0000

    Day 4: Initial implementation with grid

---
+++ src/bin/day04.rs
@@ -0,0 +1,89 @@
+use aoc2025::{Grid, get_input_as_str};
+use std::str::FromStr;
+
+#[inline(always)]
+fn remove_rolls(grid: &mut Grid) -> usize {
+    let removable_rolls: Vec<usize> = grid
+        .iter()
+        .filter(|p| p.value() == b'@')
+        .filter(|p| p.adjacent_iter().filter(|n| n.value() == b'@').count() < 4)
+        .map(|p| p.index())
+        .collect();
+
+    let removed_rolls = removable_rolls.len();
+
+    removable_rolls
+        .into_iter()
+        .for_each(|index| grid.set_index(index, b'.'));
+
+    removed_rolls
+}
+
+#[inline(always)]
+fn day4(input: &str) -> (i64, i64) {
+    let mut grid = Grid::from_str(input).unwrap();
+
+    let (first, total) = std::iter::repeat_with(|| remove_rolls(&mut grid))
+        .take_while(|&n| n != 0)
+        .fold((None::<usize>, 0usize), |(first, total), n| {
+            (first.or(Some(n)), total + n)
+        });
+
+    (first.unwrap_or(0) as i64, total as i64)
+}
+
+#[inline(always)]
+fn main() {
+    let input = get_input_as_str();
+    let (p1, p2) = day4(input);
+    println!("{p1}\n{p2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    const SAMPLE_INPUT: &str = indoc! {"
+            ..@@.@@@@.
+            @@@.@.@.@@
+            @@@@@.@.@@
+            @.@@@@..@.
+            @@.@@@@.@@
+            .@@@@@@@.@
+            .@.@.@.@@@
+            @.@@@.@@@@
+            .@@@@@@@@.
+            @.@.@@@.@.
+        "};
+
+    #[test]
+    fn test_remove_rools() {
+        let mut grid = Grid::from_str(SAMPLE_INPUT).unwrap();
+
+        // Puzzle test cases.
+        assert_eq!(remove_rolls(&mut grid), 13);
+        assert_eq!(remove_rolls(&mut grid), 12);
+        assert_eq!(remove_rolls(&mut grid), 7);
+        assert_eq!(remove_rolls(&mut grid), 5);
+        assert_eq!(remove_rolls(&mut grid), 2);
+        assert_eq!(remove_rolls(&mut grid), 1);
+        assert_eq!(remove_rolls(&mut grid), 1);
+        assert_eq!(remove_rolls(&mut grid), 1);
+        assert_eq!(remove_rolls(&mut grid), 1);
+        assert_eq!(remove_rolls(&mut grid), 0);
+
+        // Extra one to ensure "off the end" behaviour.
+        assert_eq!(remove_rolls(&mut grid), 0);
+    }
+
+    #[test]
+    fn test_day4() {
+        const SAMPLE_PART1_ANSWER: i64 = 13;
+        const SAMPLE_PART2_ANSWER: i64 = 43;
+
+        let (part1_answer, part2_answer) = day4(SAMPLE_INPUT);
+        assert_eq!(part1_answer, SAMPLE_PART1_ANSWER, "Part 1 is incorrect");
+        assert_eq!(part2_answer, SAMPLE_PART2_ANSWER, "Part 2 is incorrect");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::sync::OnceLock;
 use std::{env, fs};
+use std::str::FromStr;
 
 #[global_allocator]
 static GLOBAL: System = System;
@@ -104,7 +105,150 @@ impl<'a> Iterator for UniformInputIterator<'a> {
             return None;
         }
 
-        self.offset =  start + self.stride;
+        self.offset = start + self.stride;
         Some(unsafe { self.bytes.get_unchecked(start..end) })
+    }
+}
+
+pub struct Grid {
+    bytes: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+
+pub struct GridPoint<'a> {
+    grid: &'a Grid,
+    x: usize,
+    y: usize,
+    value: &'a u8,
+}
+
+impl<'a> GridPoint<'a> {
+    #[inline(always)]
+    pub fn value(&self) -> u8 {
+        *self.value
+    }
+
+    #[inline(always)]
+    pub fn value_ref(&self) -> &'a u8 {
+        self.value
+    }
+
+    #[inline(always)]
+    pub fn index(&self) -> usize {
+        self.y * self.grid.width + self.x
+    }
+
+    #[inline(always)]
+    pub fn adjacent_iter(&self) -> impl Iterator<Item = GridPoint<'a>> + '_ {
+        #[rustfmt::skip]
+        const OFFSETS: [(isize, isize); 8] = [
+            (-1, -1), (0, -1), (1, -1),
+            (-1,  0), /* GP */ (1,  0),
+            (-1,  1), (0,  1), (1,  1),
+        ];
+
+        OFFSETS.into_iter().filter_map(move |(dx, dy)| {
+            let nx = self.x as isize + dx;
+            let ny = self.y as isize + dy;
+
+            if nx < 0 || ny < 0 || nx >= self.grid.width as isize || ny >= self.grid.height as isize
+            {
+                return None;
+            }
+
+            let x = nx as usize;
+            let y = ny as usize;
+            let idx = y * self.grid.width + x;
+            let value = unsafe {
+                // Safety: Indices have already been validated above.
+                self.grid.bytes.get_unchecked(idx)
+            };
+
+            Some(GridPoint {
+                grid: self.grid,
+                x,
+                y,
+                value,
+            })
+        })
+    }
+}
+
+impl FromStr for Grid {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self::from_uniform_input_iter(UniformInputIterator::from_bytes(s.as_bytes())))
+    }
+}
+
+impl Grid {
+    pub fn from_uniform_input_iter(iter: UniformInputIterator<'_>) -> Self {
+        let mut bytes = vec![];
+        let mut lines = 0;
+        let line_length = OnceLock::new();
+
+        for line in iter {
+            line_length.get_or_init(|| line.len());
+            bytes.extend_from_slice(line);
+            lines += 1;
+        }
+
+        Self {
+            bytes,
+            width: *line_length.get().unwrap(),
+            height: lines,
+        }
+    }
+
+    pub fn iter(&self) -> GridPointIterator<'_> {
+        GridPointIterator {
+            grid: self,
+            x: 0,
+            y: 0,
+        }
+    }
+
+    #[inline(always)]
+    pub fn set_index(&mut self, idx: usize, v: u8) {
+        self.bytes[idx] = v;
+    }
+}
+
+pub struct GridPointIterator<'a> {
+    grid: &'a Grid,
+    x: usize,
+    y: usize,
+}
+
+impl<'a> Iterator for GridPointIterator<'a> {
+    type Item = GridPoint<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y >= self.grid.height {
+            return None;
+        }
+
+        let index = self.y * self.grid.width + self.x;
+        let value = unsafe {
+            // Safety: These indices have been previously checked to be in-range.
+            self.grid.bytes.get_unchecked(index)
+        };
+
+        let point = GridPoint {
+            grid: self.grid,
+            x: self.x,
+            y: self.y,
+            value,
+        };
+
+        self.x += 1;
+        if self.x >= self.grid.width {
+            self.x = 0;
+            self.y += 1;
+        }
+
+        Some(point)
     }
 }
